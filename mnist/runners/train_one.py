0df8fdc7035078b2b8d566f0b3149310e70f87c9
Timothy Moore <mtimothy984@gmail.com>
2019-04-12 19:34:10 -0700

    try less deep mnist network
    otherwise was taking forever

--- mnist/runners/train_one.py
+++ mnist/runners/train_one.py
@@ -38,11 +38,8 @@ def main():
     test_pwl = MNISTData.load_test().to_pwl().restrict_to(set(range(10))).rescale()
 
     layers_and_nonlins = (
-        (784, 'relu'),
-        (3920, 'relu'),
-        (1960, 'relu'),
-        (490, 'relu'),
         (200, 'relu'),
+        (150, 'relu'),
         (90, 'tanh'),
         (90, 'tanh'),
         (90, 'tanh'),
