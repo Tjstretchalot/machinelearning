151d768a20b357e5ee0fbba3e338644990b0464d
Timothy Moore <mtimothy984@gmail.com>
2019-04-25 08:50:35 -0700

    Add convutils and try complex mnist network

---
+++ mnist/runners/train_one_complex.py
@@ -0,0 +1,107 @@
+"""Trains a single feedforward model on the classifications task
+"""
+
+import shared.setup_torch #pylint: disable=unused-import
+from shared.models.ff import FeedforwardComplex, FFTeacher
+from shared.models.ff import ComplexLayer as CL
+import shared.trainer as tnr
+import shared.measures.dist_through_time as dtt
+import shared.measures.pca_ff as pca_ff
+import shared.measures.pca_3d as pca_3d
+import shared.measures.pca3d_throughtrain as pca3d_throughtrain
+import shared.measures.saturation as satur
+import shared.measures.participation_ratio as pr
+import shared.measures.svm as svm
+import shared.filetools
+import shared.npmp as npmp
+import shared.convutils as cu
+import torch
+import torch.nn
+
+from mnist.pwl import MNISTData
+import os
+
+
+SAVEDIR = shared.filetools.savepath()
+
+INPUT_DIM = 28*28 # not modifiable
+OUTPUT_DIM = 3
+
+def main():
+    """Entry point"""
+    train_pwl = MNISTData.load_train().to_pwl().restrict_to(set(range(10))).rescale()
+    test_pwl = MNISTData.load_test().to_pwl().restrict_to(set(range(10))).rescale()
+
+    nets = cu.FluentShape(28*28)
+    network = FeedforwardComplex(
+        INPUT_DIM, OUTPUT_DIM,
+        [
+            nets.unflatten_conv_(1, 28, 28),
+            nets.conv_(1, 5, 5),
+            nets.relu(),
+            nets.maxpool_(2),
+            nets.flatten_(invokes_callback=True),
+            nets.linear_(200),
+            nets.tanh(),
+            nets.linear_(OUTPUT_DIM),
+            nets.tanh()
+        ]
+    )
+
+    layer_names = ('input', 'conv2d-relu', 'maxpool', 'tanh', 'output')
+
+    trainer = tnr.GenericTrainer(
+        train_pwl=train_pwl,
+        test_pwl=test_pwl,
+        teacher=FFTeacher(),
+        batch_size=45,
+        learning_rate=0.001,
+        optimizer=torch.optim.Adam([p for p in network.parameters() if p.requires_grad], lr=0.001),
+        criterion=torch.nn.CrossEntropyLoss()
+    )
+
+    pca3d_throughtrain.FRAMES_PER_TRAIN = 2
+    pca3d_throughtrain.SKIP_TRAINS = 4
+    pca3d_throughtrain.NUM_FRAME_WORKERS = 6
+
+    dig = npmp.NPDigestor('train_one_complex', 35)
+
+    dtt_training_dir = os.path.join(SAVEDIR, 'dtt')
+    pca_training_dir = os.path.join(SAVEDIR, 'pca')
+    pca3d_training_dir = os.path.join(SAVEDIR, 'pca3d')
+    pr_training_dir = os.path.join(SAVEDIR, 'pr')
+    svm_training_dir = os.path.join(SAVEDIR, 'svm')
+    satur_training_dir = os.path.join(SAVEDIR, 'saturation')
+    trained_net_dir = os.path.join(SAVEDIR, 'trained_model')
+    pca_throughtrain_dir = os.path.join(SAVEDIR, 'pca_throughtrain')
+    (trainer
+     .reg(tnr.EpochsTracker())
+     .reg(tnr.EpochsStopper(3))
+     .reg(tnr.DecayTracker())
+     .reg(tnr.DecayStopper(8))
+     .reg(tnr.LRMultiplicativeDecayer())
+     .reg(tnr.DecayOnPlateau())
+     .reg(tnr.AccuracyTracker(5, 1000, True))
+     .reg(tnr.OnEpochCaller.create_every(dtt.during_training_ff(dtt_training_dir, True, dig), skip=100))
+     #.reg(tnr.OnEpochCaller.create_every(pca_3d.during_training(pca3d_training_dir, True, dig, plot_kwargs={'layer_names': layer_names}), skip=100))
+     #.reg(tnr.OnEpochCaller.create_every(pca_ff.during_training(pca_training_dir, True, dig), skip=100))
+     .reg(tnr.OnEpochCaller.create_every(pr.during_training_ff(pr_training_dir, True, dig), skip=100))
+     .reg(tnr.OnEpochCaller.create_every(svm.during_training_ff(svm_training_dir, True, dig), skip=100))
+     .reg(tnr.OnEpochCaller.create_every(satur.during_training(satur_training_dir, True, dig), skip=100))
+     .reg(tnr.OnEpochCaller.create_every(tnr.save_model(trained_net_dir), skip=100))
+     .reg(pca3d_throughtrain.PCAThroughTrain(pca_throughtrain_dir, layer_names, True))
+     .reg(tnr.OnFinishCaller(lambda *args, **kwargs: dig.join()))
+     .reg(tnr.ZipDirOnFinish(dtt_training_dir))
+     .reg(tnr.ZipDirOnFinish(pca_training_dir))
+     .reg(tnr.ZipDirOnFinish(pca3d_training_dir))
+     .reg(tnr.ZipDirOnFinish(pr_training_dir))
+     .reg(tnr.ZipDirOnFinish(svm_training_dir))
+     .reg(tnr.ZipDirOnFinish(satur_training_dir))
+     .reg(tnr.ZipDirOnFinish(trained_net_dir))
+    )
+
+    trainer.train(network)
+    dig.archive_raw_inputs(os.path.join(SAVEDIR, 'digestor_raw.zip'))
+
+if __name__ == '__main__':
+    main()
---
+++ shared/convutils.py
@@ -0,0 +1,184 @@
+"""This module makes it easier to work with convolutional layers"""
+
+import torch
+import torch.nn
+from functools import reduce
+import operator
+from shared.models.ff import ComplexLayer
+
+def flatten_after_maxpool(hidacts):
+    """A valid operation on the hidacts that flattens after maxpooling"""
+    return hidacts.reshape(-1, reduce(operator.mul, hidacts.shape[1:]))
+
+class FluentShape:
+    """a fluent api for reshaping from maxpooling
+
+    Attributes:
+        dims (n-tuple): the shape we currently have.
+    """
+
+    def __init__(self, dims):
+        if isinstance(dims, int):
+            self.dims = (dims,)
+        elif isinstance(dims, (list, torch.Size)):
+            self.dims = tuple(dims)
+        elif isinstance(dims, tuple):
+            self.dims = dims
+        else:
+            raise ValueError(f'expected dims is tuple, got {dims} (type={type(dims)})')
+
+        for idx, val in enumerate(self.dims):
+            if not isinstance(val, int):
+                raise ValueError(f'expected dims[{idx}] is int, got {val} (type={type(val)})')
+
+
+    def copy_(self, other):
+        """Copies the dims from the given fluent shape into this one and returns this"""
+        if isinstance(other, FluentShape):
+            self.dims = other.dims
+        elif isinstance(other, tuple):
+            self.dims = other
+        elif isinstance(other, (list, torch.Size)):
+            self.dims = tuple(other)
+        elif isinstance(other, int):
+            self.dims = (other,)
+        else:
+            raise ValueError(f'unknown fluentshape style: {other} (type={type(other)})')
+
+        for idx, val in enumerate(self.dims):
+            if not isinstance(val, int):
+                raise ValueError(f'expected other[{idx}] is int, got {val} (type={type(val)})')
+
+        return self
+
+    def unflatten_conv(self, channels, width, height):
+        """Unflattens a flattened shape into a convolvable one with the given number
+        of channels, width, and height"""
+        if len(self.dims) != 1:
+            raise ValueError(f'cannot unflatten non-flat {self.dims}')
+
+        if self.dims[0] != channels*width*height:
+            raise ValueError(f'cannot unflatten {self.dims} to {channels}, {width}, {height} (flattened that is {channels*width*height})')
+
+        return FluentShape((channels, height, width))
+
+    def unflatten_conv_(self, channels, width, height):
+        """In-place unflatten for convolution and return the appropriate complex layer
+        that performs this operation
+        """
+        newme = self.unflatten_conv(channels, width, height)
+        lyr = ComplexLayer(
+            style='other', is_module=False, invokes_callback=False,
+            action=lambda x: x.reshape(-1, channels, width, height))
+        self.copy_(newme)
+        return lyr
+
+
+    def conv(self, out_channels, kernel_size, stride=1, padding=0):
+        """Returns a new fluent shape that is the convolution of this with the given
+        kernel size"""
+        if isinstance(kernel_size, int):
+            kernel_size = (kernel_size, kernel_size)
+        if isinstance(stride, int):
+            stride = (stride, stride)
+        if isinstance(padding, int):
+            padding = (padding, padding)
+
+        if len(self.dims) != 3:
+            raise ValueError(f'cannot convolve this without reshaping to (C_in, H, W)')
+
+        kernel_width, kernel_height = kernel_size
+        stride_width, stride_height = stride
+        padding_width, padding_height = padding
+        _, height, width = self.dims
+
+        out_height = int((height - kernel_height + 2 * padding_height)/stride_height) + 1
+        out_width = int((width - kernel_width + 2 * padding_width)/stride_width) + 1
+
+        return FluentShape((out_channels, out_height, out_width))
+
+    def conv_(self, out_channels, kernel_size, stride=1, padding=0, invokes_callback=False):
+        """In-place convolve dimensions and return appropriate ComplexLayer"""
+
+        newme = self.conv(out_channels, kernel_size, stride, padding)
+        lyr = ComplexLayer(
+            style='layer', is_module=True, invokes_callback=invokes_callback,
+            action=torch.nn.Conv2d(
+                in_channels=self.dims[0],
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding))
+
+        self.copy_(newme)
+        return lyr
+
+    def maxpool(self, kernel_size, stride=1):
+        """Returns a new fluent shape that is this maxpooled with the given kernel size and
+        stride"""
+        if isinstance(kernel_size, int):
+            kernel_size = (kernel_size, kernel_size)
+        if isinstance(stride, int):
+            stride = (stride, stride)
+
+        if len(self.dims) != 3:
+            raise ValueError(f'need to have dim (C, H, W) to maxpool but have dim {self.dims}')
+
+        channels, height, width = self.dims
+        kernel_width, kernel_height = kernel_size
+        stride_width, stride_height = stride
+
+        out_height = int((height - kernel_height) / stride_height) + 1
+        out_width = int((width - kernel_width) / stride_width) + 1
+
+        return FluentShape((channels, out_height, out_width))
+
+    def maxpool_(self, kernel_size, stride=1, invokes_callback=True):
+        """Inplace maxpool2d and return appropriate ComplexLayer"""
+        newme = self.maxpool(kernel_size, stride)
+        lyr = ComplexLayer(
+            style='layer', is_module=True, invokes_callback=invokes_callback,
+            action=torch.nn.MaxPool2d(kernel_size=kernel_size, stride=stride))
+        self.copy_(newme)
+        return lyr
+
+    def flatten(self):
+        """Returns a flattened version of this shape"""
+        return FluentShape(reduce(operator.mul, self.dims))
+
+    def flatten_(self, invokes_callback=False):
+        """Flattens this shape in place and returns the appropriate layer to flatten
+        with"""
+        newme = self.flatten()
+        lyr = ComplexLayer(
+            style='other', is_module=False, invokes_callback=invokes_callback,
+            action=flatten_after_maxpool
+        )
+        self.copy_(newme)
+        return lyr
+
+    def linear_(self, output_dim, invokes_callback=False):
+        """Inplace linear layer to output dim and return appropriate ComplexLayer"""
+        if not isinstance(output_dim, int):
+            raise ValueError(f'expected output_dim is int, got {output_dim} (type={type(output_dim)})')
+        if len(self.dims) != 1:
+            raise ValueError(f'cannot do all-to-all when not flat (have dims {self.dims})')
+
+        lyr = ComplexLayer(style='layer', is_module=True, invokes_callback=invokes_callback,
+               action=torch.nn.Linear(self.dims[0], output_dim))
+        self.dims = (output_dim,)
+        return lyr
+
+    def tanh(self, invokes_callback=True):
+        """Convenience function that produces a tanh nonlinearity complex layer"""
+        return ComplexLayer(
+            style='nonlinearity', is_module=False,
+            invokes_callback=invokes_callback,
+            action=torch.tanh)
+
+    def relu(self, invokes_callback=True):
+        """Convenience function that produces a relu nonlinearity complex layer"""
+        return ComplexLayer(
+            style='nonlinearity', is_module=False,
+            invokes_callback=invokes_callback,
+            action=torch.relu)
