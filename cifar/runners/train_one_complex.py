5ad0fc6dcf612c5a09fd55dff12ab45d6a5ca0a0
Timothy Moore <mtimothy984@gmail.com>
2019-05-02 09:20:32 -0700

    fix missing input label

--- cifar/runners/train_one_complex.py
+++ cifar/runners/train_one_complex.py
@@ -78,7 +78,7 @@ def main():
     test_pwl = CIFARData.load_test().to_pwl().restrict_to(set(range(10))).rescale()
 
     layer_names = ('input', 'conv3+maxpool', 'FC -> 1000 (tanh)', 'FC -> 1000 (tanh)', 'FC -> 500 (tanh)', 'FC -> 250 (tanh)', 'FC -> 100 (tanh)', 'FC -> 100 (tanh)', 'FC -> 100 (tanh)', f'FC -> {OUTPUT_DIM} (tanh)')
-    plot_layers = tuple(i for i, _ in enumerate(layer_names))
+    plot_layers = tuple(i for i in range(1, len(layer_names)))
     trainer = tnr.GenericTrainer(
         train_pwl=train_pwl,
         test_pwl=test_pwl,
