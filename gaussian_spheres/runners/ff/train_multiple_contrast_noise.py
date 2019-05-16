59b67c658b0cb3e87ec8958cea62a38918caa61e
Timothy Moore <mtimothy984@gmail.com>
2019-05-16 12:09:54 -0700

    increase LR

--- gaussian_spheres/runners/ff/train_multiple_contrast_noise.py
+++ gaussian_spheres/runners/ff/train_multiple_contrast_noise.py
@@ -57,7 +57,7 @@ def train_with_noise(vari, rep, ignoreme): # pylint: disable=unused-argument
         nonlinearity=nonlins
     )
 
-    _lr = 0.001
+    _lr = 0.01
     trainer = tnr.GenericTrainer(
         train_pwl=train_pwl,
         test_pwl=test_pwl,
