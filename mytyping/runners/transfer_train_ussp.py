e66dc076c995164b3f32c543642f2a0a5b22cd5a
Timothy Moore <mtimothy984@gmail.com>
2019-05-23 16:26:32 -0700

    determine when to increase network size based on accuracy

--- mytyping/runners/transfer_train_ussp.py
+++ mytyping/runners/transfer_train_ussp.py
@@ -77,32 +77,31 @@ def train_on(network, teacher, wordlist, num_words, thisdir, patience):
         .reg(mtnr.AccuracyTracker(5, 100, True, verbose=False))
     )
 
-    trainer.train(network)
+    result = trainer.train(network)
 
     _eval(ssp, teacher, network)
 
+    return result['accuracy']
+
 def main():
     """Meant to be invoked for this runner"""
+    start_block = 0
     num_blocks = 50
     block_size = 10
-    small_block_size = 5
-    small_num_blocks = block_size / small_block_size
-    start_block = 0 # set to 0 for a clean model
-    start_small_block = 0 # set to 0 for a clean model
-    encoding_dim_exp = [1, 2, 4, 1, 0] * ((num_blocks // block_size) + 1)
-    context_dim_exp = [1, 0, 0, 0, 3] * ((num_blocks // block_size) + 1)
-    decoding_dim_exp = [1, 2, 4, 1, 0] * ((num_blocks // block_size) + 1)
-    patience = [45 for _ in range(num_blocks)]
-
-    if small_num_blocks != int(small_num_blocks):
-        raise ValueError(f'small_num_blocks={small_num_blocks}')
-    small_num_blocks = int(small_num_blocks)
+    acc_low_changes = (
+        (1, 0, 1),
+        (1, 0, 1),
+        (1, 0, 1),
+        (0, 1, 0)
+    )
+    acc_low_ch_ind = 0
+    patience = 45
 
     total_wordlist = mwords.load_custom('data/commonwords/google-10000-english-no-swears.txt').subset(num_blocks * block_size)
     if len(total_wordlist.words) != num_blocks * block_size:
         raise ValueError(f'not enough words (got only {len(total_wordlist.words)}, not {num_blocks * block_size})')
 
-    if start_block == 0 and start_small_block == 0:
+    if start_block == 0:
         network = ss1.EncoderDecoder(
             input_dim=menc.INPUT_DIM,
             encoding_dim=8,
@@ -113,26 +112,21 @@ def main():
             decoding_layers=1
         )
     else:
-        network = torch.load(os.path.join(SAVEDIR, str(start_block), str(start_small_block), 'trained_models', 'epoch_finished.pt'))
+        network = torch.load(os.path.join(SAVEDIR, str(start_block), 'trained_models', 'epoch_finished.pt'))
 
     teacher = ss1.EncoderDecoderTeacher(menc.stop_failer, 30)
-    for large_block in range(start_block, num_blocks+1):
-        if large_block == start_block:
-            miter = range(start_small_block + 1, small_num_blocks)
-        else:
-            miter = range(small_num_blocks)
-        for small_block in miter:
-            folder = os.path.join(SAVEDIR, str(large_block), str(small_block))
-            num_words = large_block * block_size + small_block * small_block_size
-            train_on(network, teacher, total_wordlist, num_words, folder, patience[large_block])
-
-        enc_amt = encoding_dim_exp[large_block]
-        ctx_amt = context_dim_exp[large_block]
-        dec_amt = decoding_dim_exp[large_block]
-        print(f'Network size increasing by {enc_amt}, {ctx_amt}, {dec_amt}')
-        network = network.transfer_up(network.encoding_dim + enc_amt,
-                                      network.context_dim + ctx_amt,
-                                      network.decoding_dim + dec_amt)
+    for block in range(start_block, num_blocks):
+        folder = os.path.join(SAVEDIR, str(block))
+        num_words = block_size * (block + 1)
+        acc = train_on(network, teacher, total_wordlist, num_words, folder, patience)
+        while acc < 0.95:
+            enc_amt, ctx_amt, dec_amt = acc_low_changes[acc_low_ch_ind]
+            acc_low_ch_ind = (acc_low_ch_ind + 1) % len(acc_low_changes)
+            print(f'Network size increasing by {enc_amt}, {ctx_amt}, {dec_amt}')
+            network = network.transfer_up(network.encoding_dim + enc_amt,
+                                          network.context_dim + ctx_amt,
+                                          network.decoding_dim + dec_amt)
+            acc = train_on(network, teacher, total_wordlist, num_words, folder, patience)
 
 if __name__ == '__main__':
     main()
